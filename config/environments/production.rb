86e02849ff8cb0162029bdbd942fcce940578c9c
Bryce Anderson <keeputahweird@gmail.com>
2011-11-12 12:57:48 -0700

    adding deploy/load_assets to deploy script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Sorcery::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 

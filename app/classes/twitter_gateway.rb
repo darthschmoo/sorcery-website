55448b01d5971c187bd7b83ea7fed7d30e3888b7
Bryce Anderson <keeputahweird@gmail.com>
2012-03-16 18:05:20 -0600

    Hrm..

---
+++ app/classes/twitter_gateway.rb
@@ -0,0 +1,26 @@
+# TODO: be able to set up and revoke oauth tokens?
+# getting the oauth token in the first place:   http://obi-akubue.org/?p=479
+module TwitterGateway  
+  def self.setup_gateway
+    return if @gw_set_up
+    
+    Twitter.configure do |config|
+      oauth = Sorcery.config.twitter.oauth
+      config.consumer_key       = oauth.consumer_key
+      config.consumer_secret    = oauth.consumer_secret
+      config.oauth_token        = oauth.token
+      config.oauth_token_secret = oauth.secret
+    end
+    
+    @gw_set_up = true
+  end
+
+  
+  setup_gateway
+
+  def self.write( tweet )
+    response = Twitter.update( tweet )
+    debugger
+    puts "Hello" 
+  end
+end
---
+++ app/helpers/tweet_quotes_helper.rb
@@ -0,0 +1,2 @@
+module TweetQuotesHelper
+end

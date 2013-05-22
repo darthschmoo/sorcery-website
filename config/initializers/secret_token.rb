39a8cfd584222506c348e7698720ac4ce2b47ef8
Bryce Anderson <keeputahweird@gmail.com>
2013-05-21 18:46:57 -0600

    that should never have been committed.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sorcery::Application.config.secret_token = "c6be9d9ec785be98072f9dc477d3271329435ade5289bff15bf95c52f1e3a699c45a03b386d1a86597a31bfa25eb091edd8d2f78481e495e3b0a65dd04f745c2"
